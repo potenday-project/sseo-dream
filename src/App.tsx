5131ff7ef8c52ccb5555528cf5375a34a0929b9b
bereal1995 <gygud98@gmail.com>
2023-12-10 22:59:59 +0900

    api 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,60 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-
-import viteLogo from '../public/vite.svg';
+import axios from 'axios';
 
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [value, setValue] = useState('');
+
+  const fetchData = async (message: string) => {
+    setIsLoading(true);
+    const responseData = await axios.post('https://1x2ep04v87.apigw.ntruss.com/test/hh-test/rFI3X7i7x7/json', {
+      body: {
+        messages: [
+          {
+            role: 'user',
+            content: message,
+          },
+        ],
+        maxTokens: 2048,
+      },
+      headers: {
+        'Content-Type': 'text/stream',
+        'X-NCP-CLOVASTUDIO-API-KEY':
+          'NTA0MjU2MWZlZTcxNDJiY25sZ68/c8HYjZvTffUOPq1/Hqp+MWTF3kFsGPPtDnLpp1yw5+wTxZDwAtBYdWCtZNoOHxIOXF8OVN0sRur81hsRzuiJ2dXbkjCofAG06yO/VER2WXjtfM6t9F7NfYEljdEURHMdIl8gItT8VZSYYgQRBUvbLmTSMdmGsc4n6TmTrSI4/VajPXlEcWAmv1JGwcjqBC/ptE3z8Ja+Sg6OxqY=',
+        'X-NCP-APIGW-API-KEY': 'fwc41Q4gKDC6MOwyL8p5NhKg4yLldocu3O093o3Y',
+        'X-NCP-CLOVASTUDIO-REQUEST-ID': '904c68b46fd24443b51be9245293b352',
+      },
+    });
+
+    setData(responseData);
+    setIsLoading(false);
+  };
+
+  const handleClickButton = async () => {
+    await fetchData(value);
+  };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((prevCount) => prevCount + 1)}>
-          count is {count}
+        <input type="text" placeholder="질문입력" onChange={(e) => setValue(e.target.value)} />
+        <button type="button" onClick={handleClickButton}>
+          질문
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button
+          type="button"
+          onClick={() => {
+            setValue('');
+            setData({});
+          }}
+        >
+          초기화
+        </button>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <div>{isLoading ? '답변중...' : `답변: ${data?.data?.result?.message?.content ?? ''}`}</div>
     </>
   );
 }
