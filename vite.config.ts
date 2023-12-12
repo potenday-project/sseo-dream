6f73b51fc97a86cb717c1c9c685797c0635c35c2
bereal1995 <gygud98@gmail.com>
2023-12-13 00:09:31 +0900

    svgr 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      include: '**/*.svg',
+    }),
+  ],
 });
