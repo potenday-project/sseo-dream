a986297d890a0e358012b15978ee406c61d4db98
bereal1995 <gygud98@gmail.com>
2023-12-13 00:09:50 +0900

    로고 컴포넌트 추가

---
+++ src/components/Logo.tsx
@@ -0,0 +1,13 @@
+import LogoTitle from '../assets/logo-title.svg';
+import LogoSymbol from '../assets/logo-symbol.svg';
+
+function Logo() {
+  return (
+    <div className="inline-flex flex-col items-center gap-[24px] text-heading dark:text-heading">
+      <LogoSymbol />
+      <LogoTitle />
+    </div>
+  );
+}
+
+export default Logo;
