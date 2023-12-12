2716d8a5b445583af14b80da905b1de9102ed83a
bereal1995 <gygud98@gmail.com>
2023-12-13 00:23:56 +0900

    기본 컬러 light로 수정

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import LogoSymbol from '../assets/logo-symbol.svg';
 
 function Logo() {
   return (
-    <div className="inline-flex flex-col items-center gap-[24px] text-heading dark:text-heading">
+    <div className="inline-flex flex-col items-center gap-[24px] text-heading dark:text-heading-dark">
       <LogoSymbol />
       <LogoTitle />
     </div>
