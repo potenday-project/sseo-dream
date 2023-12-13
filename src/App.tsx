b153bd2c8dfd56abe64328946fe529d9e3a41248
FEWoobIn <hwb0218@gmail.com>
2023-12-14 00:29:35 +0900

    reacr-router 설정 후 메인페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,9 @@
-import { useState } from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import axios from 'axios';
+import routes from './routes';
 
-import './App.css';
+const router = createBrowserRouter(routes);
 
 export default function App() {
-  const [data, setData] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
-  const [value, setValue] = useState('');
-
-  console.log(import.meta.env.VITE_CLOVASTUDIO_HOST);
-
-  const fetchData = async (message: string) => {
-    setIsLoading(true);
-    const responseData = await axios.post(
-      'https://ifofflnpn5.apigw.ntruss.com/open_api/oepn_api_prod/info/json',
-      { body: { message } },
-      { params: { person: 'friends' } },
-    );
-
-    console.log(responseData);
-
-    setData(responseData);
-    setIsLoading(false);
-  };
-
-  const handleClickButton = async () => {
-    await fetchData(value);
-  };
-
-  return (
-    <>
-      <div>
-        <input type="text" placeholder="질문입력" onChange={(e) => setValue(e.target.value)} />
-        <button type="button" onClick={handleClickButton}>
-          질문
-        </button>
-        <button
-          type="button"
-          onClick={() => {
-            setValue('');
-            setData({});
-          }}
-        >
-          초기화
-        </button>
-      </div>
-      <div>{isLoading ? '답변중...' : `답변: ${data?.data?.result?.message?.content ?? ''}`}</div>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,5 @@
+import FunnelTest from '../components/Funnel/FunnelTest';
+
+export default function MainPage() {
+  return <FunnelTest />;
+}
