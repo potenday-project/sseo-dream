a043b90f6e52a0f6c8a3eaeb44e109bdf6bb0155
nmnnhlba <xorwnsnmnn@naver.com>
2023-12-15 01:04:02 +0900

    update PurposeFragment style

--- src/components/PurposeFragment.tsx
+++ src/components/PurposeFragment.tsx
@@ -7,11 +7,10 @@ export default function PurposeFragment({ title, contents, svgName }: Purpose) {
     <div
       key={title}
       className="flex flex-col m-1 items-center justify-center
-      w-full h-60 bg-neutral-700 rounded-3xl text-center text-white"
-    >
+      w-full h-60 bg-neutral-700 rounded-3xl text-center text-white">
       <img src={svgFileName} alt={title} />
       <div className="m-1 text-lg font-bold font-['Pretendard']">{title}</div>
-      <div className="text-[15px] font-light font-['Pretendard']">{contents}</div>
+      <div className="text-sm font-light font-['Pretendard']">{contents}</div>
     </div>
   );
 }
