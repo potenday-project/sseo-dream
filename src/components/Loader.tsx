74ac28565d684f1f7a1f6f23ac38b88c08ce09dc
bereal1995 <gygud98@gmail.com>
2023-12-16 05:06:48 +0900

    로더 추가

---
+++ src/components/Loader.tsx
@@ -0,0 +1,16 @@
+import loadingImage from '../assets/loading-icon.gif';
+
+function Loader() {
+  return (
+    <div className="fixed left-0 top-0 flex items-center justify-center w-full h-full bg-background">
+      <div className="flex flex-col items-center justify-center gap-[8px]">
+        <div className="w-[137px] h-[137px]">
+          <img src={loadingImage} alt="loading" />
+        </div>
+        <div className="text-base font-light">정성껏 써드리고 있어요</div>
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
