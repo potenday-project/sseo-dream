d1db03dbaf3f3fb000283d3aef918c27b97fd943
FEWoobIn <hwb0218@gmail.com>
2023-12-15 21:43:05 +0900

    토스트 애니메이션 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
   theme: {
+    extend: {
+      colors: {
+        success: {
+          wrapper: '#EDFBD8',
+          color: '#2B641E',
+        },
+        warning: {
+          wrapper: '#FEF7D1',
+          color: '#755118',
+        },
+        error: {
+          wrapper: '#FCE8DB',
+          color: '#71192F',
+        },
+      },
+      keyframes: {
+        'fade-in-out': {
+          '0%': { opacity: 0 },
+          '25%': { opacity: 1 },
+          '90%': { opacity: 1 },
+          '100%': { opacity: 0 },
+        },
+      },
+      animation: {
+        fadeInOut: 'fade-in-out 3s ease-out forwards',
+      },
+    },
   },
+  safelist: [
+    {
+      pattern: /(bg|text)-(success|warning|error)-(wrapper|color)/,
+      variants: ['hover', 'focus', 'active'],
+    },
+  ],
   plugins: [],
 };
