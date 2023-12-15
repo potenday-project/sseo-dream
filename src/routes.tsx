3b18cdece2956c9e80bc08c073cd1d306bcccba8
bereal1995 <gygud98@gmail.com>
2023-12-16 00:46:06 +0900

    결과페이지 라우터 추가

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,16 @@
 import { RouteObject } from 'react-router-dom';
 
 import MainPage from './pages/MainPage';
+import ResultPage from './pages/ResultPage';
 
 const routes: RouteObject[] = [
   {
     path: '/',
     element: <MainPage />,
+  },
+  {
+    path: '/result',
+    element: <ResultPage />,
   },
 ];
 
