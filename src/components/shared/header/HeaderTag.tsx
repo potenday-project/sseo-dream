590f802b4f6eebf24f3a92a5a3d9590171b436e9
FEWoobIn <hwb0218@gmail.com>
2023-12-17 00:18:48 +0900

    header파일 폴더 위치 벼녕

--- src/components/shared/header/HeaderTag.tsx
+++ src/components/shared/header/HeaderTag.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 
 export default function HeaderTag({ children }: PropsWithChildren) {
   return (
-    <div className="px-2.5 py-1.5 rounded-2xl text-sm font-bold bg-yellow-200 text-zinc-800">
+    <div className="px-2.5 py-1.5 rounded-2xl text-sm font-bold text-font-light bg-primary">
       {children}
     </div>
   );
