2a671f19026f73e71200ebe4aa103876d0c06455
FEWoobIn <hwb0218@gmail.com>
2023-12-17 02:07:09 +0900

    입력폼 이벤트 핸들러 바인딩

--- src/components/shared/input/Input.tsx
+++ src/components/shared/input/Input.tsx
@@ -1,10 +1,22 @@
-import { PropsWithChildren } from 'react';
+import { ChangeEvent, PropsWithChildren } from 'react';
 
-export default function Input({ children }: PropsWithChildren) {
+type InputProps = {
+  value: string;
+  name?: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+};
+
+export default function Input({ children, name, value, onChange }: PropsWithChildren<InputProps>) {
   return (
     <div className="w-full my-5 flex flex-col gap-3">
       <div className="font-bold text-white">{children}</div>
-      <input type="text" className="w-full h-14 p-3 rounded-md text-white bg-contentBackground" />
+      <input
+        type="text"
+        name={name}
+        value={value}
+        onChange={(e) => onChange(e)}
+        className="w-full h-14 p-3 rounded-md text-white bg-contentBackground"
+      />
     </div>
   );
 }
