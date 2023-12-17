e03e28290df0723b0d4e4b0de39412035e5bf6d3
FEWoobIn <hwb0218@gmail.com>
2023-12-17 13:44:45 +0900

    스플래시 텍스트 수정

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -4,7 +4,7 @@ function SplashScreen() {
   return (
     <div className="left-0 top-0 right-0 bottom-0 absolute flex flex-col items-center justify-center bg-background-dark animate-splashFadeOut">
       <Logo />
-      <div className="font-base">써드림 서비스 한줄 설명</div>
+      <div className="pt-5 font-base">당신의 마음을 써드림</div>
     </div>
   );
 }
