7159413e4ebfb6630e9cee84f5dc5f4fc60765da
nmnnhlba <xorwnsnmnn@naver.com>
2023-12-17 12:45:02 +0900

    update working/personal valueList

--- src/components/funnel/Step2.tsx
+++ src/components/funnel/Step2.tsx
@@ -6,37 +6,31 @@ import PrevNextButtons from '../PrevNextButtons';
 import { StepProps } from './types';
 import { useLetterStore } from '../../stores/letter';
 
-const WORKING_VALUE_LIST = [
-  '기념일/명절',
-  '인사/안부',
-  '축하/감사',
-  '안내/공지',
-  '요청/제안',
-  '문의/답변',
-  '사과/수습',
-  '그외',
-];
-
-const PERSONAL_VALUE_LIST = [
-  '기념일/명절',
-  '인사/안부',
-  '축하/감사',
-  '위로/응원',
-  '사과/반성',
-  '그외',
-];
+const VALUE_LIST = {
+  working: [
+    '기념일/명절',
+    '인사/안부',
+    '축하/감사',
+    '안내/공지',
+    '요청/제안',
+    '문의/답변',
+    '사과/수습',
+    '그외',
+  ],
+  personal: ['기념일/명절', '인사/안부', '축하/감사', '위로/응원', '사과/반성', '그외'],
+};
 
 export default function Step2({ onClickPrev, onClickNext }: StepProps) {
-  const { purpose } = useLetterStore.getState().userSelectionResult;
+  const { userSelectionResult } = useLetterStore();
 
-  let VALUE_LIST: string[];
+  let valueList: string[] = [];
 
-  if (purpose === '업무') {
-    VALUE_LIST = WORKING_VALUE_LIST;
-  } else if (purpose === '개인') {
-    VALUE_LIST = PERSONAL_VALUE_LIST;
+  if (userSelectionResult.purpose === '업무') {
+    valueList = VALUE_LIST.working;
+  } else if (userSelectionResult.purpose === '개인') {
+    valueList = VALUE_LIST.personal;
   } else {
-    VALUE_LIST = ['그외'];
+    valueList = ['그외'];
   }
 
   return (
@@ -44,7 +38,7 @@ export default function Step2({ onClickPrev, onClickNext }: StepProps) {
       <Title sequence={1} sequenceShown>
         어떤 글을 써드릴까요?
       </Title>
-      <ValueList valueList={VALUE_LIST} selectionKey="type" etcValue="그외 어떤 글을 써드릴까요?" />
+      <ValueList valueList={valueList} selectionKey="type" etcValue="그외 어떤 글을 써드릴까요?" />
       <PrevNextButtons selectionKey="type" onClickPrev={onClickPrev} onClickNext={onClickNext} />
     </MediumCategoryWrapper>
   );
